a791a09d3a087b90ece1afa93067391ba4670f83
IhO17 <107650639+IhO17@users.noreply.github.com>
2023-09-08 03:09:03 +0200

    print library arr

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-//let book1 = new Book("Golum", "JRR", 302, "not read");
-//let book2 = new Book("HP", "Rofling", 452, "read");
-//
-//addBookToLibrary(book1);
-//addBookToLibrary(book2);
-//
-//console.log(myLibrary)
+let book1 = new Book("Golum", "JRR", 302, "not read");
+let book2 = new Book("HP", "Rofling", 452, "read");
+let book3 = new Book("PEP", "Rofling", 452, "read");
+
+let book4 = new Book("Kskek", "Rofling", 452, "read");
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+addBookToLibrary(book4);
+
 
 const showButton = document.getElementById("showDialogButton");
 const bookDialog = document.getElementById("modal");
 const confirmButton = document.querySelector("#confirm-btn");
-const bookSection = document.querySelector("books");
-//const ConfirmButton = bookDialog.querySelector("#confirm-btn");
+const bookSection = document.querySelector("#books");
+
 
 showButton.addEventListener("click", () => {
     bookDialog.showModal();
@@ -35,5 +36,28 @@ showButton.addEventListener("click", () => {
 
 confirmButton.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(e);
+    bookDialog.close();
 });
+ 
+
+myLibrary.forEach(book => {
+    
+    const addbook = document.createElement("div");
+    addbook.style.backgroundColor = "#3b82f6";
+    addbook.style.borderRadius = "15px";
+    addbook.style.color = "white";
+    addbook.style.fontSize = "1.7rem";
+    addbook.style.padding = "30px 20px 20px 30px";
+    addbook.classList.add(`book-${book.title}`);
+    bookSection.appendChild(addbook);
+    const bookWindow = document.querySelector(`.book-${book.title}`);
+
+    for (let value in book) {
+        
+        const addline = document.createElement("div");
+        addline.style.margin = "10px";
+        addline.style.textAlign = "center";
+        addline.textContent = `${book[value]}`;
+        bookWindow.appendChild(addline);
+    }
+})
