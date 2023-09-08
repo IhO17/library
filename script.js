061a6a8a002c2f3f2c7830dbde05e446e0288e20
IhO17 <107650639+IhO17@users.noreply.github.com>
2023-09-08 11:56:09 +0200

    add css for bookDiv buttons

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const confirmButton = document.querySelector("#confirm-btn");
 const bookSection = document.querySelector("#books");
 const cancelButton = document.querySelector("#cancel-btn");
 
+
 showButton.addEventListener("click", () => {
     bookDialog.showModal();
     document.getElementById("myForm").reset();
@@ -32,41 +33,6 @@ cancelButton.addEventListener("click", (e) => {
     bookDialog.close();
 });
 
-function printBook(title, author, pages, read) {
-    const addbook = document.createElement("div");
-    addbook.style.backgroundColor = "#3b82f6";
-    addbook.style.borderRadius = "15px";
-    addbook.style.color = "white";
-    addbook.style.fontSize = "1.7rem";
-    addbook.style.padding = "20px 20px 10px 30px";
-    addbook.classList.add(`book-${title.value}`);
-    bookSection.appendChild(addbook);
-    const bookWindow = document.querySelector(`.book-${title.value}`);
-    console.log(`book-${title}`);
-
-    for (x in arguments) {
-        let addline = document.createElement("div");
-        addline.style.margin = "10px";
-        addline.style.textAlign = "center";
-        addline.textContent = `${arguments[x].value}`;
-        bookWindow.appendChild(addline);
-    }
-
-    let addbutton = document.createElement("button");
-    addbutton.classList = "read-btn";
-    addbutton.style.height = "38px";
-    addbutton.style.width = "120px";
-    addbutton.style.marginTop = "30px";
-    addbutton.style.fontSize = "1.5rem";
-    addbutton.style.backgroundColor = "#fde047";
-    addbutton.style.borderRadius = "10px"; 
-    addbutton.style.color = "black";
-
-    
-    bookWindow.appendChild(addbutton);
-
-}
-
 function confirmBtn(e) {
     e.preventDefault();
     
@@ -78,6 +44,99 @@ function confirmBtn(e) {
     //addBookToLibrary(book);
     printBook(title, author, pages, read);
     bookDialog.close();
+}
+
+function printBook(title, author, pages, read) {
+    const addbook = document.createElement("div");
+    addbook.style.backgroundColor = "#fbda4a";
+    addbook.style.borderRadius = "15px";
+    addbook.style.color = "black";
+    addbook.style.fontSize = "1.7rem";
+    addbook.style.padding = "20px 20px 10px 30px";
+    addbook.classList.add(`book-${title.value}`);
+    bookSection.appendChild(addbook);
+    const bookWindow = document.querySelector(`.book-${title.value}`);
+
+    for (x in arguments) {
+        let addline = document.createElement("div");
+        addline.style.margin = "10px";
+        addline.style.textAlign = "center";
+        if (arguments[x] === pages) {
+            addline.textContent = `${arguments[x].value} pages`;
+        } else {
+            addline.textContent = `${arguments[x].value}`;
+        }
+
+        //if (arguments[x] === read && ) {
+        //    addline.textContent = `READ`;
+        //} else if (arguments[x] === read && ) {
+        //    addline.textContent = `NOT READ`;
+        //}
+        bookWindow.appendChild(addline);
+    }
+
+    let buttonDiv = document.createElement("div");
+    buttonDiv.classList = "btn-div";
+    buttonDiv.style.display = "flex";
+    buttonDiv.style.justifyContent = "space-around";
+
+    bookWindow.appendChild(buttonDiv);
+    let addBtn = document.querySelector(".btn-div");
+
+    let addbutton = document.createElement("button");
+    let addDelete = document.createElement("button");
+    
+    styleButton(addbutton);
+    styleButton(addDelete);
+    addDelete.style.backgroundColor = "#dc2626";
+    addDelete.classList = "delete-btn";
+    addDelete.textContent = "Delete";
+    addbutton.textContent = "Read";
+
+    addBtn.appendChild(addbutton);
+    addBtn.appendChild(addDelete);
+
+    const readButton = document.querySelector(".read-btn");
+    const deleteButton = document.querySelector(".delete-btn");
+
+    readButton.addEventListener("click", () => {
+        //add function to change state of READ
+        console.log("heh");
+    })
+
+    //add another button for deletion
+    
+    const ReButton = document.querySelector('.read-btn');
+    const DelButton = document.querySelector('.delete-btn');
+
+    
+    ReButton.addEventListener('mouseover', () => {
+        ReButton.style.backgroundColor = "#16a34a8b";
+    })
+
+    ReButton.addEventListener('mouseout', () => {
+        ReButton.style.backgroundColor = "#16a34a";
+    })
+
+    DelButton.addEventListener('mouseover', () => {
+        DelButton.style.backgroundColor = "#dc262662";
+    })
+
+    DelButton.addEventListener('mouseout', () => {
+        DelButton.style.backgroundColor = "#dc2626";
+    })
+    
+}
+
+function styleButton(button) {   
+    button.classList = "read-btn";
+    button.style.height = "38px";
+    button.style.width = "120px";
+    button.style.marginTop = "30px";
+    button.style.fontSize = "1.3rem";
+    button.style.color = "white";
+    button.style.backgroundColor = "#16a34a";
+    button.style.borderRadius = "10px";
 }
 
 //function printLibrary() {
@@ -110,4 +169,3 @@ function confirmBtn(e) {
 //            bookSection.removeChild("div");
 //    })
 //}
-
